776637ed69804d35bf08284d0d8ba48f81a24ddc
iainrm290 <murdochiain17@gmail.com>
2024-03-25 14:44:52 +0000

    add_db_tiles_s_to_x

--- db/data/tiles.js
+++ db/data/tiles.js
@@ -548,4 +548,181 @@ module.exports = [
             }
         }
     },
+    {
+        grid_id: { x: 0, y: 0, z: 0},
+        orientation: 0,
+        corresponding_tiles: {
+            north: null,
+            east: null,
+            south: null,
+            west: null
+        },
+        tile_type: 'S',
+        is_monestary: false,
+        citizen: {
+            is_citizen: false,
+            asset: null,
+            location: null
+        },
+        assets: {
+            north: {
+                asset: 'city',
+                connects: ['east', 'west']
+                
+            },
+            east: {
+                asset: 'city',
+                connects: ['north', 'west']
+            },
+            south: {
+                asset: "road",
+                connects: []
+            },
+            west: {
+                asset: "city",
+                connects: ['north', 'east']
+            }
+        }
+    },
+    {
+        grid_id: { x: 0, y: 0, z: 0},
+        orientation: 0,
+        corresponding_tiles: {
+            north: null,
+            east: null,
+            south: null,
+            west: null
+        },
+        tile_type: 'T',
+        is_monestary: false,
+        citizen: {
+            is_citizen: false,
+            asset: null,
+            location: null
+        },
+        assets: {
+            north: {
+                asset: 'city',
+                connects: ['east', 'west']
+                
+            },
+            east: {
+                asset: 'city',
+                connects: ['north', 'west']
+            },
+            south: {
+                asset: "road",
+                connects: []
+            },
+            west: {
+                asset: "city",
+                connects: ['north', 'east']
+            }
+        }
+    },
+    {
+        grid_id: { x: 0, y: 0, z: 0},
+        orientation: 0,
+        corresponding_tiles: {
+            north: null,
+            east: null,
+            south: null,
+            west: null
+        },
+        tile_type: 'U',
+        is_monestary: false,
+        citizen: {
+            is_citizen: false,
+            asset: null,
+            location: null
+        },
+        assets: {
+            north: {
+                asset: 'road',
+                connects: ['south']
+                
+            },
+            east: {},
+            south: {
+                asset: "road",
+                connects: ['north']
+            },
+            west: {}
+        }
+    },
+    {
+        grid_id: { x: 0, y: 0, z: 0},
+        orientation: 0,
+        corresponding_tiles: {
+            north: null,
+            east: null,
+            south: null,
+            west: null
+        },
+        tile_type: 'V',
+        is_monestary: false,
+        citizen: {
+            is_citizen: false,
+            asset: null,
+            location: null
+        },
+        assets: {
+            north: {},
+            east: {},
+            south: {
+                asset: "road",
+                connects: ['west']
+            },
+            west: {
+                asset: "road",
+                connects: ['south']
+            }
+        }
+    },
+    {
+        grid_id: { x: 0, y: 0, z: 0},
+        orientation: 0,
+        corresponding_tiles: {
+            north: null,
+            east: null,
+            south: null,
+            west: null
+        },
+        tile_type: 'W',
+        is_monestary: false,
+        citizen: {
+            is_citizen: false,
+            asset: null,
+            location: null
+        },
+        assets: {
+            north: {},
+            east: {},
+            south: {},
+            west: {}
+        }
+    },
+    {
+        grid_id: { x: 0, y: 0, z: 0},
+        orientation: 0,
+        corresponding_tiles: {
+            north: null,
+            east: null,
+            south: null,
+            west: null
+        },
+        tile_type: 'X',
+        is_monestary: false,
+        citizen: {
+            is_citizen: false,
+            asset: null,
+            location: null
+        },
+        assets: {
+            north: {},
+            east: {},
+            south: {},
+            west: {}
+        }
+    }
 ]
