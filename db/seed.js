e971acadd47f0b252741af196ecfab74ae0e79c0
FrinkoTay <francis.taylor20027@gmail.com>
2024-03-26 14:21:17 +0000

    add example.js in hidden file and add functionality to drop tile database before reseeding

--- db/seed.js
+++ db/seed.js
@@ -3,12 +3,19 @@ const data = require('./data/tiles')
 
 seedTiles = () => {
     const database = client.db('gameboard')
+
+    database.collection("tiles").drop(function(err, success) {
+        if (err) throw err;
+        if (success) console.log("Collection deleted");
+    });
+
     const tiles = database.collection('tiles')
 
     console.log('before seeding')
     return tiles.insertMany(data)
     .then(() => {
         console.log("tiles collection seeded")
+        //database.close()
     })
 }
 
