80ea867e11c4c0467245a7dac380e64cda8b0bed
iainrm290 <murdochiain17@gmail.com>
2024-04-02 13:17:37 +0100

    Added post new user endpoint

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const apiRouter = require('./routers/api-router')
+const bodyParser = require('body-parser')
 
 const app = express()
 
 app.use(cors())
+app.use(bodyParser.json())
 
 //app.use(express.json())
 
--- routers/api-router.js
+++ routers/api-router.js
@@ -2,6 +2,8 @@ const apiRouter = require('express').Router()
 const tilesRouter = require('./tiles-router')
 const usersRouter = require('./users-router')
 
+
+
 apiRouter.use('/tiles', tilesRouter)
 
 apiRouter.use('/users', usersRouter)
